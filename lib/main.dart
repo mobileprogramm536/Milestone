dddd47e4d3cf21514ccc3471a737b7e3d1037bb6
selin nur güzel <selin.guz.13@gmail.com>
2024-11-08 16:24:57 +0300

    1234

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:milestone/pages/forgot_password.dart';
+import 'package:milestone/pages/codeVerificationScreen.dart';
+import 'package:milestone/pages/register%20page.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(fontFamily: 'Poppins', fontSize: 18),
+          bodyMedium: TextStyle(fontFamily: 'Poppins', fontSize: 16),
+          bodySmall: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+          headlineLarge: TextStyle(
+              fontFamily: 'Poppins', fontWeight: FontWeight.bold, fontSize: 24),
+          headlineMedium: TextStyle(
+              fontFamily: 'Poppins', fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/register',
+      routes: {
+        '/register': (context) => RegisterPage(),
+        '/forgotPassword': (context) => forgotPassword(),
+        '/codeVerificationScreen': (context) => codeVerificationScreen(),
+      },
+    ));
