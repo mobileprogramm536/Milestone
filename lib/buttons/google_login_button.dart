2505be6411b39911727cbac2198804508c2e7ad7
selin nur güzel <98047810+selingzl@users.noreply.github.com>
2024-11-12 13:57:01 +0300

    Revert "Google ile giriş metodu eklenmedi"

--- lib/buttons/google_login_button.dart
+++ lib/buttons/google_login_button.dart
@@ -1,42 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleButton extends StatelessWidget {
   final VoidCallback onPressed;
 
   GoogleButton({required this.onPressed});
 
-  Future<void> _signInWithGoogle() async {
-    try {
-      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-      if (googleUser != null) {
-        final GoogleSignInAuthentication googleAuth =
-            await googleUser.authentication;
-
-        final AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleAuth.accessToken,
-          idToken: googleAuth.idToken,
-        );
-
-        await FirebaseAuth.instance.signInWithCredential(credential);
-        print("Google Sign-In successful.");
-      }
-    } catch (e) {
-      print("Google Sign-In failed: $e");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
 
     return OutlinedButton.icon(
-      onPressed: () async {
-        await _signInWithGoogle();
-        onPressed();
-      },
+      onPressed: onPressed,
       icon: Icon(Icons.g_mobiledata, color: Colors.white, size: height * 0.03),
       label: Text(
         'Google',
