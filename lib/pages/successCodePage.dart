95a3d6374973f0c44089c40bf264bc5295e62a9b
Baris Gezer <barisgezer@protonmail.com>
2024-12-01 03:28:20 +0300

    Change name

--- lib/pages/successCodePage.dart
+++ lib/pages/successCodePage.dart
@@ -23,23 +23,37 @@ class _SuccessScreenState extends State<SuccessScreen> {
           ),
         ),
         child: Center(
-          child: Container(
-            width: 80,
-            height: 80,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.greenAccent,
-                width: 2,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.greenAccent,
+                    width: 2,
+                  ),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.greenAccent,
+                    size: 40,
+                  ),
+                ),
               ),
-            ),
-            child: Center(
-              child: Icon(
-                Icons.check,
-                color: Colors.greenAccent,
-                size: 40,
+              SizedBox(height: 16), // İcon ile text arasında boşluk bırakır
+              Text(
+                'Mail Gonderildi!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
