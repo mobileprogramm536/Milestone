8bea71b6b0420b3e0b934a713a47758dbe267951
selin nur güzel <selin.guz.13@gmail.com>
2025-01-05 03:30:50 +0300

    card design

--- lib/cards/exploreMore_page_card.dart
+++ lib/cards/exploreMore_page_card.dart
@@ -22,16 +22,17 @@ class _ExploremorePageCardState extends State<ExploremorePageCard> {
   RouteCard? routeC = null;
   int likes = 0; // To track the number of likes
   late bool isLiked; // To toggle like state
+
   @override
   void initState() {
-    super.initState(); // Initialize likes
+    super.initState();
     isLiked = false; // Default state: unliked
-    RouteService().getRouteCard(widget.routeId).then((element) => {
-          setState(() {
-            routeC = element;
-            likes = routeC!.likecount!;
-          })
-        });
+    RouteService().getRouteCard(widget.routeId).then((element) {
+      setState(() {
+        routeC = element;
+        likes = routeC?.likecount ?? 0; // If null, default to 0
+      });
+    });
   }
 
   void toggleLike() {
@@ -41,151 +42,170 @@ class _ExploremorePageCardState extends State<ExploremorePageCard> {
     });
   }
 
+  @override
   Widget build(BuildContext context) {
-    if (routeC == null) {
-      return Container(
-        color: Colors.red,
-        height: 50,
-        width: 50,
-      );
-    }
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+
+    if (routeC == null) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          color: AppColors.grey1,
+          height: height * 0.1,
+          width: height * 0.1,
+        ),
+      );
+    }
+
     return SizedBox(
       height: height * 0.21,
       width: width * 0.47,
-      child: Stack(children: [
-        Positioned(
-          top: height * 0.03,
-          child: Container(
-            height: height * 0.18,
-            width: width * 0.46,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              color: AppColors.darkgrey2,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: height * 0.003),
-                    Text(
-                      routeC!.title!,
-                      style: const TextStyle(
-                        color: AppColors.white1,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      routeC!.description!,
-                      style: const TextStyle(
-                        color: AppColors.white1,
-                        fontSize: 6.0,
-                      ),
-                      maxLines: 4,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(
-                      height: height * 0.01,
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.pin_drop_outlined,
-                          color: AppColors.yellow1,
-                          size: 16.0,
-                        ),
-                        const SizedBox(width: 2.0),
-                        Text(
-                          routeC!.location!,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 12.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.navigation_outlined,
-                          color: AppColors.yellow1,
-                          size: 16.0,
-                        ),
-                        const SizedBox(width: 4.0),
-                        Text(
-                          routeC!.destinationcount!.toString(),
-                          style: const TextStyle(
-                            color: AppColors.white1,
-                            fontSize: 12.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+      child: Stack(
+        children: [
+          Positioned(
+            top: height * 0.032,
+            child: Container(
+              height: height * 0.18,
+              width: width * 0.46,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-              ),
-            ),
-          ),
-        ),
-        Stack(
-          children: [
-            Container(
-              width: width * 0.47,
-              height: height * 0.06,
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  color: AppColors.darkgrey2,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
+                color: AppColors.grey1,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(
-                        width: width * 0.12,
-                      ),
+                      SizedBox(height: height * 0.003),
+                      // Safe null check for routeC
                       Text(
-                        routeC!.username!,
-                        style: const TextStyle(
+                        routeC?.title ?? 'Title not available',
+                        style: TextStyle(
                           color: AppColors.white1,
-                          fontSize: 5.0,
+                          fontSize: height * 0.018,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       Text(
-                        routeC!.likecount!.toString(),
-                        style: const TextStyle(
+                        routeC?.description ?? 'Description not available',
+                        style: TextStyle(
                           color: AppColors.white1,
-                          fontSize: 8.0,
+                          fontSize: height * 0.01,
                         ),
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      IconButton(
-                        onPressed: toggleLike, // Action to toggle like
-                        icon: Icon(
-                          isLiked ? Icons.favorite : Icons.favorite_border,
-                          color: isLiked ? AppColors.red1 : AppColors.white1,
-                          size: 16.0,
-                        ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.pin_drop_outlined,
+                            color: AppColors.yellow1,
+                            size: 16.0,
+                          ),
+                          const SizedBox(width: 2.0),
+                          Text(
+                            routeC?.location ?? 'Location not available',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.navigation_outlined,
+                            color: AppColors.yellow1,
+                            size: 16.0,
+                          ),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            routeC?.destinationcount?.toString() ?? '0',
+                            style: const TextStyle(
+                              color: AppColors.white1,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ),
               ),
             ),
-            Positioned(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/femaleavatar9.png'),
-                radius: 20.0,
+          ),
+          Stack(
+            children: [
+              Container(
+                width: width * 0.47,
+                height: height * 0.06,
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    color: AppColors.grey1,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        SizedBox(width: width * 0.12),
+                        Expanded(
+                          child: Text(
+                            routeC?.username ?? 'Username not available',
+                            style: TextStyle(
+                              color: AppColors.white1,
+                              fontSize: height * 0.01,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: width * 0.01,
+                        ),
+                        Text(
+                          routeC?.likecount?.toString() ?? '0',
+                          style: const TextStyle(
+                            color: AppColors.white1,
+                            fontSize: 10.0,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: toggleLike, // Action to toggle like
+                          icon: Icon(
+                            isLiked ? Icons.favorite : Icons.favorite_border,
+                            color: isLiked ? AppColors.red1 : AppColors.white1,
+                            size: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
-      ]),
+              Positioned(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.yellow, // Sarı arka plan rengi
+                    shape: BoxShape.circle, // Yuvarlak şekil
+                  ),
+                  padding: EdgeInsets.all(3.0),
+                  child: CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/images/femaleavatar9.png'),
+                    radius: 20.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/cards/explore_page_card.dart
+++ lib/cards/explore_page_card.dart
@@ -23,6 +23,7 @@ class _ExploreCardState extends State<ExploreCard> {
   RouteCard? routeC = null;
   int likes = 0; // To track the number of likes
   late bool isLiked; // To toggle like state
+
   @override
   void initState() {
     super.initState(); // Initialize likes
@@ -44,129 +45,157 @@ class _ExploreCardState extends State<ExploreCard> {
 
   @override
   Widget build(BuildContext context) {
-    if (routeC == null) {
-      return Container(
-        color: Colors.red,
-        height: 50,
-        width: 50,
-      );
-    }
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+
+    if (routeC == null) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          color: AppColors.grey1,
+          height: height * 0.1,
+          width: height * 0.1,
+        ),
+      );
+    }
+
     return GestureDetector(
       onTap: () => {},
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        color: AppColors.darkgrey2, // Background color as in the design
-        margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-        child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Circular image on the left
-                    Padding(
-                      padding: EdgeInsets.all(2.0),
-                      child: GestureDetector(
-                        onTap: () => {},
-                        child: CircleAvatar(
-                          backgroundImage:
-                              AssetImage('assets/images/femaleavatar9.png'),
-                          radius: 30.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: width * 0.02,
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: height * 0.005,
-                          ),
-                          Text(
-                            routeC!.title!,
-                            style: const TextStyle(
-                              color: AppColors.white1,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: height * 0.001),
-                          // Description
-                          Text(
-                            routeC!.description!,
-                            style: const TextStyle(
-                              color: AppColors.white1,
-                              fontSize: 8.0,
-                            ),
-                            maxLines: 4,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                Center(
-                  child: Row(
+      child: Container(
+        width: width * 0.5,
+        height: height * 0.21,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          color: AppColors.grey1, // Background color as in the design
+          margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+          child: Padding(
+              padding: EdgeInsets.all(height * 0.018),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Icon(
-                        Icons.pin_drop_outlined,
-                        color: AppColors.yellow1,
-                        size: 16.0,
-                      ),
-                      const SizedBox(width: 4.0),
-                      Text(
-                        routeC!.location!,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 12.0,
+                      // Circular image on the left
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.yellow, // Sarı arka plan rengi
+                            shape: BoxShape.circle, // Yuvarlak şekil
+                          ),
+                          padding: EdgeInsets.all(
+                              3.0), // Arka planın boyutunu büyütüyoruz
+                          child: GestureDetector(
+                            onTap: () => {},
+                            child: CircleAvatar(
+                              backgroundImage:
+                                  AssetImage('assets/images/femaleavatar9.png'),
+                              radius: 30.0, // Avatarın boyutu
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(width: width * 0.03),
-                      const Icon(
-                        Icons.navigation_outlined,
-                        color: AppColors.yellow1,
-                        size: 16.0,
+                      SizedBox(
+                        width: width * 0.05,
                       ),
-                      const SizedBox(width: 4.0),
-                      Text(
-                        routeC!.destinationcount!.toString(),
-                        style: const TextStyle(
-                          color: AppColors.white1,
-                          fontSize: 12.0,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: height * 0.005,
+                            ),
+                            Text(
+                              routeC!.title!,
+                              style: TextStyle(
+                                color: AppColors.white1,
+                                fontSize: height * 0.023,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: height * 0.001),
+                            // Description
+                            Text(
+                              routeC!.description!,
+                              style: TextStyle(
+                                  color: AppColors.white1,
+                                  fontSize: height * 0.013,
+                                  fontWeight: FontWeight.w100),
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
                         ),
-                      ),
-                      SizedBox(width: width * 0.04),
-                      Row(children: [
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  Center(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: width * 0.01,
+                        ),
+                        const Icon(
+                          Icons.pin_drop_outlined,
+                          color: AppColors.yellow1,
+                          size: 16.0,
+                        ),
+                        const SizedBox(width: 4.0),
                         Text(
-                          routeC!.likecount!.toString(),
+                          routeC!.location!,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 12.0,
+                          ),
+                        ),
+                        SizedBox(width: width * 0.08),
+                        Transform.rotate(
+                          angle: 90 * 3.1415927 / 180, // 90 derece döndürme
+                          child: const Icon(
+                            Icons.navigation_outlined,
+                            color: AppColors.yellow1,
+                            size: 16.0,
+                          ),
+                        ),
+                        const SizedBox(width: 4.0),
+                        Text(
+                          routeC!.destinationcount!.toString(),
                           style: const TextStyle(
                             color: AppColors.white1,
                             fontSize: 12.0,
                           ),
                         ),
-                        IconButton(
-                          onPressed: toggleLike, // Action to toggle like
-                          icon: Icon(
-                            isLiked ? Icons.favorite : Icons.favorite_border,
-                            color: isLiked ? AppColors.red1 : AppColors.white1,
-                            size: 24.0,
+                        SizedBox(width: width * 0.17),
+                        Row(children: [
+                          Text(
+                            routeC!.likecount!.toString(),
+                            style: const TextStyle(
+                              color: AppColors.white1,
+                              fontSize: 12.0,
+                            ),
                           ),
-                        ),
-                      ]),
-                    ],
-                  ),
-                )
-              ],
-            )),
+                          IconButton(
+                            onPressed: toggleLike, // Action to toggle like
+                            icon: Icon(
+                              isLiked ? Icons.favorite : Icons.favorite_border,
+                              color:
+                                  isLiked ? AppColors.red1 : AppColors.white1,
+                              size: 24.0,
+                            ),
+                          ),
+                        ]),
+                      ],
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
